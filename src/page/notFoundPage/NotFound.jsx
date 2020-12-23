1b887153e87e75e83e69705355bde57cf397f38b
Nazar <createSlice>
2020-12-23 14:16:20 +0200

    testProjectPush

---
+++ src/page/notFoundPage/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const NotFoundPage = () => {
+    return (
+        <div className={styles.wrapper}>
+            <h1>404</h1>
+            <h2>oops! page not found</h2>
+        </div>
+    )
+}
---
+++ src/page/users/prts/tab/TabBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const Tab = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
